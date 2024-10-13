e95c49f2f5851bcd68b0c4c9534b12d144f6bf3c
playoffthecuff <jmobile@bk.ru>
2024-10-14 02:33:45 +0300

    feat: add cd command

--- file-manager/index.js
+++ file-manager/index.js
@@ -1,7 +1,8 @@
 import {argv, env, exit, stdin, stdout} from "process"
 import readline from "readline"
 import {homedir} from "os"
-import {dirname} from "path"
+import {dirname, resolve} from "path"
+import { access, stat } from "fs";
 
 const userName = argv[2]?.startsWith('--username=') ? argv[2].split('=')[1] : env.npm_config_username ? env.npm_config_username : 'Guest'
 const rl = readline.createInterface({
@@ -11,6 +12,8 @@ const rl = readline.createInterface({
 
 let dir = homedir()
 
+const showCD = () => console.info(`You are currently in ${dir},`, `Enter command`)
+
 const end = () => {
   console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
   rl.close()
@@ -19,24 +22,36 @@ const end = () => {
 
 const up = () => {
   dir = dirname(dir)
+  showCD()
+}
+
+const cd = (p) => {
+  stat(resolve(dir, p), (e,s) => {
+    if (e) console.error('Operation failed -', e.message)
+    else if (!s.isDirectory()) console.warn("Operation failed - destination isn't directory")
+    else dir = resolve(dir, p)
+    showCD()
+  })
 }
 
 const commands = {
   ".exit": end,
   "up": up,
+  "cd": cd,
 }
 
 console.log(`Welcome to the File Manager, ${userName}!`)
-console.log(`You are currently in ${dir},`, `Enter command` )
+showCD()
 
 rl.on('SIGINT', () => end())
 rl.on('line', i => {
   i = i.trim()
   try {
-    commands[i] ? commands[i]() : console.log('Invalid input')
+    const cmd = i.replace(/ [\s\S]+/, '')
+    commands[cmd] ? commands[cmd](...i.split(' ').slice(1)) : console.warn('Invalid input')
   } catch (e) {
-    console.log('Operation failed')
+    console.warn('Operation failed')
+    // console.log(e)
   }
-  console.log(`You are currently in ${dir},`, `Enter command`)
 })
 
