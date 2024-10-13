67d318ff217f97c347f52262a4d86f590de2a7d7
playoffthecuff <jmobile@bk.ru>
2024-10-13 18:47:55 +0300

    feat: add current dir path message

--- file-manager/index.js
+++ file-manager/index.js
@@ -7,12 +7,18 @@ const rl = readline.createInterface({
   output: stdout
 })
 
+console.log(`Welcome to the File Manager, ${userName}!`)
+const dir = import.meta.dirname
+console.log(`You are currently in ${dir}`)
+
 rl.on('SIGINT', () => end())
-rl.on('line', (i) => i.trim() === '.exit' && end())
+rl.on('line', i => {
+  console.log(`You are currently in ${dir}`)
+  i.trim() === '.exit' && end()
+})
 
 const end = () => {
   console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
   rl.close()
   exit(0)
 }
-console.log(`Welcome to the File Manager, ${userName}!`)
