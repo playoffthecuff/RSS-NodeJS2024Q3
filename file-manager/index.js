09e0e3279d58692b3762478f5aaa45007a365e04
playoffthecuff <jmobile@bk.ru>
2024-10-13 18:40:03 +0300

    feat: add farewell

--- file-manager/index.js
+++ file-manager/index.js
@@ -1,2 +1,18 @@
-const userName = process.argv[2]?.startsWith('--username=') ? process.argv[2].split('=')[1] : process.env.npm_config_username ? process.env.npm_config_username : 'Guest'
+import {argv, env, exit, stdin, stdout} from "process"
+import readline from "readline"
+
+const userName = argv[2]?.startsWith('--username=') ? argv[2].split('=')[1] : env.npm_config_username ? env.npm_config_username : 'Guest'
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout
+})
+
+rl.on('SIGINT', () => end())
+rl.on('line', (i) => i.trim() === '.exit' && end())
+
+const end = () => {
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
+  rl.close()
+  exit(0)
+}
 console.log(`Welcome to the File Manager, ${userName}!`)
