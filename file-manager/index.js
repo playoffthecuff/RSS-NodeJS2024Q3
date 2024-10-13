909d715a7ce5f4d79f4165a943413d434361a6ea
playoffthecuff <jmobile@bk.ru>
2024-10-14 01:03:49 +0300

    feat: add up command

--- file-manager/index.js
+++ file-manager/index.js
@@ -1,6 +1,7 @@
 import {argv, env, exit, stdin, stdout} from "process"
 import readline from "readline"
 import {homedir} from "os"
+import {dirname} from "path"
 
 const userName = argv[2]?.startsWith('--username=') ? argv[2].split('=')[1] : env.npm_config_username ? env.npm_config_username : 'Guest'
 const rl = readline.createInterface({
@@ -8,24 +9,34 @@ const rl = readline.createInterface({
   output: stdout
 })
 
+let dir = homedir()
+
 const end = () => {
   console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
   rl.close()
   exit(0)
 }
 
+const up = () => {
+  dir = dirname(dir)
+}
+
 const commands = {
   ".exit": end,
+  "up": up,
 }
 
 console.log(`Welcome to the File Manager, ${userName}!`)
-const dir = homedir()
 console.log(`You are currently in ${dir},`, `Enter command` )
 
 rl.on('SIGINT', () => end())
 rl.on('line', i => {
   i = i.trim()
-  commands[i] ? commands[i]() : console.log('Invalid input')
+  try {
+    commands[i] ? commands[i]() : console.log('Invalid input')
+  } catch (e) {
+    console.log('Operation failed')
+  }
   console.log(`You are currently in ${dir},`, `Enter command`)
 })
 
