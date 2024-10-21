b9b36a4e3cf06d492510c59bb68c079197b13f64
playoffthecuff <jmobile@bk.ru>
2024-10-21 12:10:37 +0300

    init: start dev crud-api

---
+++ crud-api/webpack.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'node:path';
+
+const config = {
+  mode: 'production',
+  target: 'node',
+  entry: resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    clean: true,
+    filename: 'index.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+    ],
+  },
+};
+
+export default config;
