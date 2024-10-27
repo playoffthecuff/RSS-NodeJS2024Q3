4de9b44fccf4b9c66755eb91fb2778c61f5e0407
playoffthecuff <jmobile@bk.ru>
2024-10-27 22:14:39 +0300

    feat: add ws connection handling

--- battleship/src/ws_server/index.ts
+++ battleship/src/ws_server/index.ts
@@ -1,5 +1,21 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
 
-const w = new WebSocketServer({port: 3000});
+const port = 3000;
 
-w.on('connection', () => console.log('WS handshake is complete'));
+const w = new WebSocketServer({ port });
+
+console.log(`WS server is running on ws://localhost:${port}`);
+
+w.on("connection", (w) => {
+  console.log("WS handshake is complete");
+});
+
+const off = () => {
+  console.log("WS server closed");
+  w.close(() => {
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", off);
+process.on("SIGTERM", off);
